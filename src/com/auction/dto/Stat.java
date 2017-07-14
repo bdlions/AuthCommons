abce644a1328a1a92f04626647498c76a8d04a56
nazmulhasan <nazmul@apurbatech.com>
2017-07-15 02:02:21 +0600

    Featured Ad Stat

---
+++ src/com/auction/dto/Stat.java
@@ -0,0 +1,72 @@
+package com.auction.dto;
+
+/**
+ *
+ * @author nazmul hasan
+ */
+public class Stat {
+    private Product product;
+    private String date;
+    private String clicks;
+    private String impressions;
+    private String ctr;
+    private double cost;
+    private Currency costUnit;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getClicks() {
+        return clicks;
+    }
+
+    public void setClicks(String clicks) {
+        this.clicks = clicks;
+    }
+
+    public String getImpressions() {
+        return impressions;
+    }
+
+    public void setImpressions(String impressions) {
+        this.impressions = impressions;
+    }
+
+    public String getCtr() {
+        return ctr;
+    }
+
+    public void setCtr(String ctr) {
+        this.ctr = ctr;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Currency getCostUnit() {
+        return costUnit;
+    }
+
+    public void setCostUnit(Currency costUnit) {
+        this.costUnit = costUnit;
+    }
+    
+}
---
+++ src/com/auction/dto/StatParams.java
@@ -0,0 +1,36 @@
+package com.auction.dto;
+
+/**
+ *
+ * @author nazmul
+ */
+public class StatParams {
+    private int productId;
+    private String startDate;
+    private String endDate;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+    
+}
